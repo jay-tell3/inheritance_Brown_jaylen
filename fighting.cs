a9f21e20c1a9c041e5134c15b3449ee37dacb65b
jay-tell3 <jaybroww@gmail.com>
2025-04-14 15:04:10 -0500

    bruh

---
+++ fighting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance_Brown_jaylen
+{
+    internal class fighting : game
+    {
+
+        public fighting(string esrbParam, string titleParam) : base(esrbParam, titleParam) 
+        {
+            Title = titleParam;
+            Esrb = esrbParam;
+        }
+        public override string Describe()
+        {
+            return $"{Title} is a fighting game rated {Esrb}";
+        }
+    }
+}
---
+++ game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance_Brown_jaylen
+{
+    abstract internal class game
+    {
+        public string Esrb { get; set; }
+
+        public string Title { get; set; }
+
+        public game(string esraParam, string titleParam)
+        { 
+            Esrb = esraParam;
+            Title = titleParam;
+
+        }
+
+        public void PlayGame() 
+        {
+            Console.WriteLine($"{Title} is staring");
+        }
+
+        public abstract string Describe();
+    }
+}

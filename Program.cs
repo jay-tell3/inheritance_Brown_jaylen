548553fb338ad91e421e21a12f0165a220034e2e
jay-tell3 <jaybroww@gmail.com>
2025-04-14 12:57:55 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace inheritance_Brown_jaylen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# inheritance_Brown_jaylen
+ 
